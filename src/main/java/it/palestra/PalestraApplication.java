71c9e3c03e9dd7b93afcfa153ecdf141daa023e7
Graziano <graziano.aloisi@gmail.com>
2023-06-07 14:01:15 +0200

    risolto problema del file di properties, uno spazio non consentiva la connessione a mariadb

--- src/main/java/it/palestra/PalestraApplication.java
+++ src/main/java/it/palestra/PalestraApplication.java
@@ -2,14 +2,17 @@ package it.palestra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
-public class PalestraApplication {
+@ComponentScan("it.palestra")
+@EnableJdbcRepositories(basePackages ="it.palestra.dao")
 
+public class PalestraApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PalestraApplication.class, args);
 	}
 	
-	
-
 }
---
+++ src/main/java/it/palestra/controller/PalestraController.java
@@ -0,0 +1,13 @@
+package it.palestra.controller;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableJpaRepositories
+public class PalestraController {
+
+	
+	
+	
+}
