97e599dba6ca03a720ec90d5121ef2bf99b5e63b
Graziano <graziano.aloisi@gmail.com>
2023-06-30 18:46:26 +0200

    Inserimento della notazione @Order(1) @Order(2) nel listner

--- src/main/java/it/palestra/dao/listner/EmailListner.java
+++ src/main/java/it/palestra/dao/listner/EmailListner.java
@@ -3,6 +3,7 @@ package it.palestra.dao.listner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import it.palestra.dao.events.CustomerEvent;
@@ -18,9 +19,18 @@ public class EmailListner {
 	
 	//Quando Inserisce Manda il messaggio
 	@EventListener
+	@Order(2)
 	public void sendMessageAfterSave(CustomerEvent evento) {
 		mail.inviaEmail(evento.getPersona());
-		log.info("Evento Intercettato!!!");
+		log.info("Evento Intercettato!!! UNO");
+	}
+	
+	//Quando Inserisce Manda il messaggio
+	@EventListener
+	@Order(1)
+	public void sendMessageAfterSaveAzienda(CustomerEvent evento) {
+		mail.inviaEmailAzienda(evento.getPersona());
+		log.info("Evento Intercettato!!! DUE");
 	}
 
 }
--- src/main/java/it/palestra/dao/service/ServiceMail.java
+++ src/main/java/it/palestra/dao/service/ServiceMail.java
@@ -1,5 +1,6 @@
 package it.palestra.dao.service;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import it.palestra.dao.entity.Persona;
@@ -9,9 +10,14 @@ import lombok.extern.java.Log;
 @Log
 public class ServiceMail {
 
+	
 	public void inviaEmail(Persona persona) {
 		log.info("Evento eseguito PROCEDURA INVIO EMAIL NUMERO UNO "+persona.nome+"-> "+persona.cognome+"-> "+persona.citta);
-		
+	}
+	
+	
+	public void inviaEmailAzienda(Persona persona) {
+		log.info("Evento eseguito PROCEDURA INVIO EMAIL NUMERO DUE "+persona.nome+"-> "+persona.cognome+"-> "+persona.citta);
 	}
 	
 
